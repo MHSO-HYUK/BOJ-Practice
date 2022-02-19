991c922b61dc11e93938674fb517cb06a1ce1a1c
MHSO-HYUK <88526660+MHSO-HYUK@users.noreply.github.com>
2022-02-19 22:56:09 +0900

    [Silver I] Title: 격자상의 경로, Time: 120 ms, Memory: 123316 KB -BaekjoonHub

---
+++ 백준/Silver/10164.격자상의 경로/격자상의 경로.py
@@ -0,0 +1,18 @@
+#10164 격자상의 경로
+from sys import stdin
+n, m, k = map(int, stdin.readline().split())
+dp = [[0 for _ in range(m+1)] for _ in range(n+1)]
+dp[0][1] = 1
+cnt = 1
+for i in range(1, n+1):
+    for j in range(1, m + 1):
+        if cnt == k:
+            px = i
+            py = j
+        cnt += 1
+        dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+        
+if k == 0:
+    print(dp[n][m])
+else:
+    print(dp[px][py] * dp[n - px + 1][m - py + 1])
