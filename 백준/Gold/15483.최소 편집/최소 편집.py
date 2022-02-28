1b9182fd6568d27826147ee8d5e6f5af5ccfe3fb
MHSO-HYUK <88526660+MHSO-HYUK@users.noreply.github.com>
2022-02-28 19:07:55 +0900

    [Gold III] Title: 최소 편집, Time: 164 ms, Memory: 133296 KB -BaekjoonHub

--- 백준/Gold/15483.최소 편집/최소 편집.py
+++ 백준/Gold/15483.최소 편집/최소 편집.py
@@ -17,5 +17,5 @@ for i in range(1, n+1):
         if a[i-1] == b[j-1]:
             dp[i][j] = dp[i-1][j-1]
         else:
-            dp[i][j] = min(dp[i-1][j-1], min(dp[i-1][j], dp[i][j-1])) + 1
+            dp[i][j] = min(dp[i-1][j-1], dp[i-1][j], dp[i][j-1]) + 1
 print(dp[n][m])
