558047f39e128ba53bd0784fa5dc082aeb6ea6bd
MHSO-HYUK <88526660+MHSO-HYUK@users.noreply.github.com>
2022-04-27 16:01:53 +0900

    [Gold IV] Title: 마법사 상어와 파이어스톰, Time: 728 ms, Memory: 128288 KB -BaekjoonHub

--- 백준/Gold/20058. 마법사 상어와 파이어스톰/마법사 상어와 파이어스톰.py
+++ 백준/Gold/20058. 마법사 상어와 파이어스톰/마법사 상어와 파이어스톰.py
@@ -1,85 +1,75 @@
 # 20058 마법사 상어와 파이어스톰
 from sys import stdin 
 from collections import deque
-def rotate(part):
-    npart = []
-    for i in range(len(part)):
-        temp = []
-        for j in range(len(part)-1, -1, -1):
-            temp.append(part[j][i])
-        npart.append(temp)
-    return npart
-
-n, q = map(int, stdin.readline().split())
-maps = [list(map(int, stdin.readline().split())) for _ in range(2**n)]
-magic = list(map(int, stdin.readline().split()))
-# maps // 얼음의 양을 의미
-# 격자를 2**l 크기로 구분하고 90도 회전 시킴
-# 얼음이 있는 칸 3개 이상과 인접하지 않은 칸은 얼음이 하나 줄음 
-magic = magic[::-1]
-dx, dy = [1, -1, 0 ,0], [0, 0, 1, -1]
-while(magic):
-    l = magic.pop()
-    nmaps = [[[] for _ in range(len(maps) // 2**l)] for _ in range(len(maps) // 2**l)]
-    
-    
-    for i in range(0, len(maps)-2**l+1, 2**l):
-        for j in range(0, len(maps)-2**l+1, 2**l):
-            for p in range(i, i+2**l):
-                temp = []
-                for q in range(j, j+2**l):
-                    temp.append(maps[p][q])
-                nmaps[i//2**l][j//2**l].append(temp)
-            nmaps[i//2**l][j//2**l] = rotate(nmaps[i//2**l][j//2**l])
-    
-    
-    maps = [[] for _ in range(2**n)]
-    for i in range(len(nmaps)):
-        for j in range(len(nmaps[i])):
-            for p in range(len(nmaps[i][j])):
-                    maps[len(nmaps[i][j])*i+p] += nmaps[i][j][p]
-                    
-                    
-    
-    ctemp = [[0 for _ in range(2**n)] for _ in range(2**n)]
-    for i in range(len(maps)):
-        for j in range(len(maps[i])):
+def separate(new_maps, l):
+    ret_maps = [[0 for _ in range(2**n)] for _ in range(2**n)]
+    sx, sy = 0, 0
+    while(1):
+        cache = []
+        for i in range(sx, sx+2**l):
+            temp = []
+            for j in range(sy, sy+2**l):
+                temp.append(new_maps[i][j])
+            cache.append(temp)
+        
+        for y in range(2**l):
             cnt = 0
-            if maps[i][j]:
-                for k in range(4):
-                    x, y = i+dx[k], j+dy[k]
-                    if 0<=x<=2**n-1 and 0<=y<=2**n-1:
-                        if maps[x][y]:
-                            cnt += 1
-                ctemp[i][j] = cnt
-                
+            for x in range(2**l-1, -1, -1):
+                ret_maps[sx+y][sy+cnt] = cache[x][y]
+                cnt += 1
+        
+        sy = sy + 2**l
+        if sy == 2**n:
+            sx += 2**l
+            sy = 0
+            if sx == 2**n:
+                break
+    
+    return ret_maps
+
+def bfs(x, y):
+    queue = deque([[x, y]])
+    visit[x][y] = 1
+    cnt = 0
+    while(queue):
+        x, y = queue.popleft()
+        cnt += 1
+        for k in range(4):
+            nx,ny = x+dx[k], y+dy[k]
+            if 0 <= nx <= 2**n-1 and 0 <= ny <= 2**n-1:
+                if not visit[nx][ny] and maps[nx][ny]:
+                    visit[nx][ny] = 1
+                    queue.append([nx, ny])
+    return cnt
+n, q = map(int, stdin.readline().split())
+maps = list(list(map(int, stdin.readline().split())) for _ in range(2**n))
+comm = deque(map(int, stdin.readline().split()))
+dx, dy= [1, -1, 0, 0], [0, 0, 1, -1]
+while(comm):
+    l = comm.popleft()
+    maps = separate(maps, l)
+    melt = [[0 for _ in range(2**n)] for _ in range(2**n)]
     for i in range(2**n):
         for j in range(2**n):
-            if ctemp[i][j] < 3 and maps[i][j]:
-                maps[i][j] -= 1
-                
-                
-ans, maxima = 0, 0
-for i in range(len(maps)):
-    ans += sum(maps[i])
-print(ans)
-
-visit = [[0 for _ in range(2**n)] for _ in range(2**n)]
-for i in range(len(maps)):
-    for j in range(len(maps[i])):
-        if not visit[i][j] and maps[i][j] > 0:
-            cnt = 0
-            queue = deque([[i, j]])
-            visit[i][j] = 1
-            while(queue):
-                x, y = queue.popleft()
-                cnt += 1
+            if maps[i][j]:
+                cnt = 0
                 for k in range(4):
-                    a, b= x+dx[k], y+dy[k]
-                    if 0<=a<=2**n-1 and 0<=b <= 2**n-1:
-                        if not visit[a][b] and maps[a][b] > 0:
-                            visit[a][b] = 1
-                            queue.append([a, b])
-                            
-        maxima = max(maxima, cnt)
+                    nx, ny = i+dx[k], j+dy[k]
+                    if 0 <= nx <= 2**n-1 and 0 <= ny <= 2**n-1:
+                        if maps[nx][ny]:
+                            cnt += 1
+                if cnt < 3:
+                    melt[i][j] += 1
+                    
+    for i in range(2**n):
+        for j in range(2**n):
+            maps[i][j] -= melt[i][j]
+
+print(sum(map(sum, maps))) 
+visit = [[0 for _ in range(2**n)] for _ in range(2**n)]
+maxima = 0
+for i in range(2**n):
+    for j in range(2**n):
+        if not visit[i][j] and maps[i][j]:
+            maxima = max(maxima, bfs(i, j))
 print(maxima)
