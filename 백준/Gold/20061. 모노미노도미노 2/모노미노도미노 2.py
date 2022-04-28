3e5178ccfb985bc66e7330742658857aa3e513bc
MHSO-HYUK <88526660+MHSO-HYUK@users.noreply.github.com>
2022-04-28 20:23:00 +0900

    [Gold II] Title: 모노미노도미노 2, Time: 352 ms, Memory: 120436 KB -BaekjoonHub

--- 백준/Gold/20061. 모노미노도미노 2/모노미노도미노 2.py
+++ 백준/Gold/20061. 모노미노도미노 2/모노미노도미노 2.py
@@ -1,184 +1,112 @@
-# 20061 모노미노도미노2
-from sys import stdin 
-from copy import deepcopy
+# 20061 모노미노도미노 2
+from sys import stdin
+from collections import deque
+def drop_tile(t, x, y):
+    # 1. 타일이 떨어진다. 
+    if t == 1: #초록 // (x, y)  파랑 // (y, x)
+        for i in range(7):
+            if i == 6 or green[i][y]: # 블럭이 있거나 바닥인 경우
+                break
+        green[i-1][y] = 1
+        for i in range(7):
+            if i == 6 or blue[x][i]: # 블럭이 있거나 바닥인 경우
+                break
+        blue[x][i-1] = 1
+    elif t == 2: # 초록 // (x, y, y+1)   파랑 // (y, y+1 / x)
+        for i in range(7):
+            if i == 6 or green[i][y] or green[i][y+1]: # 블럭이 있거나 바닥인 경우
+                break
+        green[i-1][y], green[i-1][y+1] = 1, 1
+        for i in range(7):
+            if i == 6 or blue[x][i] or blue[x][i]: # 블럭이 있거나 바닥인 경우
+                break
+        blue[x][i-1], blue[x][i-2] = 1, 1
+    else: # 초록 // (x, x+1, y)  파랑 // (y, x, x+1) 
+        for i in range(7):
+            if i == 6 or green[i][y]: # 블럭이 있거나 바닥인 경우
+                break
+        green[i-1][y], green[i-2][y] = 1, 1
+        for i in range(7):
+            if i == 6 or blue[x][i] or blue[x+1][i]: # 블럭이 있거나 바닥인 경우
+                break
+        blue[x][i-1], blue[x+1][i-1] = 1, 1
+
+        
+def check_line():
+    global score, green, blue, flag
+    gflag, bflag = 0, 0
+    gcheck = []
+    for i in range(5, -1, -1):
+        if sum(green[i]) == 4:
+            gcheck.append(i)
+    
+    cnt = 0
+    for i in gcheck:
+        i += cnt
+        score += 1
+        green = [[0, 0, 0, 0]] + green[0:i] + green[i+1:6]
+        cnt += 1
+    
+    if sum(green[0]): 
+        gflag = 2
+    elif sum(green[1]):
+        gflag = 1
+    
+    bcheck = []
+    for j in range(5, -1, -1):
+        s = 0
+        for i in range(4):
+            s += blue[i][j]
+        if s == 4:
+            bcheck.append(j)
+    
+    cnt = 0
+    for j in bcheck:
+        j += cnt
+        score += 1
+        for a in range(4):
+            blue[a] = [0] + blue[a][0:j] + blue[a][j+1:6]
+        cnt += 1
+    
+    for i in range(2):
+        for j in range(4):
+            if blue[j][i]:
+                if i == 0:
+                    bflag = 2
+                elif bflag == 0 and i == 1:
+                    bflag = 1
+        
+    if gflag:
+        if gflag == 1:
+            green = [[0, 0, 0, 0]] + green[0:5]
+        else:
+            green = [[0, 0, 0, 0] for _ in range(2)] + green[0:4]
+
+    if bflag:
+        if bflag == 1:
+            for a in range(4):
+                blue[a] = [0] + blue[a][0:5]
+        else:
+            for a in range(4):
+                blue[a] = [0, 0] + blue[a][0:4]
+
 n = int(stdin.readline())
-info = []
+comm = deque()
 for _ in range(n):
-    t, x, y = map(int, stdin.readline().split())
-    info.append([x, y, t])
-info.reverse()
-# 블록을 빨간색에 넣으면 그 위치부터 경계에 닿거나 타 블록에 닿을 떄까지 이동한다.
-# 어떤 보드의 행이 타일로 가득 찬다면 그 행의 타일은 모두 사라진다. 
-# 점수는 하나의 행이나 열이 터질때 마다 추가된다, 
-# 사라진 이후에는 테트리스 마냥 떨어진다. 
+    comm.append(list(map(int, stdin.readline().split())))
 
-# 이 경우에는 점수 없음 
-# 초록보드의 0, 1번 행에 블록이 있으면 블록이 있는 행의 수만큼 아래 행에 있는 타일이 사라지고 떨어짐
-#파란보드의 0, 1 열에 블록이 있으면 블록이 있는 열의 수만큼 오른쪽 열이 사라지고 떨어짐 
-def blue_block(maps):
-    global score, blue
-    temp = []
-    now = [[0 for _ in range(4)] for _ in range(6)]
-    for i in range(len(maps)):
-        y = maps[i][0]
-        if not now[0][y]:
-            now[0][y] = 1
-            temp.append([0, y])
-        elif now[0][y]: # 2*1칸일 때 발생 가능 
-            now[1][y] = 1
-            temp.append([1, y])
-    
-    flag = False
-    while(1):
-        if flag:
-            break
-        maps = []
-        for i in range(len(temp)):
-            x, y = temp[i][0], temp[i][1]
-            nx, ny = x + dx[0], y + dy[0]
-            if 0 <= nx <= 5 and 0 <= ny <= 3:
-                if blue[nx][ny] == 1: # 한칸 더 가면 겹침
-                    flag = True
-                else:
-                    x, y = nx, ny
-                    maps.append([x, y])
-            else:
-                flag = True   
-                
-        if not flag: # 겹치지 않았음 
-            temp = deepcopy(maps)
-            
-        else: # 겹쳐버렸음 -> temp가 새로운 블럭의 최종 목적지가 됌 
-            for i in range(len(temp)):
-                blue[temp[i][0]][temp[i][1]] = 1
-    
-    while(1):
-        bflag = False     
-        bang = []
-        for i in range(5, -1, -1):
-            if sum(blue[i]) == 4: # 모든 행이 차게 되면
-                bflag = True
-                bang.append(i)
-                blue[i] = [0, 0, 0, 0] # 터짐 
-                score += 1
-        
-        if not bflag:
-            break
-        else:
-            b = [0 for _ in range(6)]
-            for k in bang: # 행이 터진 경우에는
-                
-                blue[k] = [0, 0, 0, 0]
-                b[k] = 1
-                
-        for i in range(6):
-            if b[i] == 1: # i 행이 터진 행이라면 
-                blue[0:i+1]  = [[0, 0, 0, 0]] + blue[0:i]
-                
-                
-    cnt = 0    
-    for i in range(0, 2): 
-        if blue[i] != [0, 0, 0, 0]:
-            cnt += 1
-    if cnt == 0:
-        pass
-    elif cnt == 1:
-        blue =  [[0, 0, 0, 0]] +  blue[0:5]
-    else:
-        blue = [[0, 0, 0, 0]] + [[0, 0, 0, 0]]  + blue[0:4]
-
-        
-        
-def green_block(maps):
-    global score, green
-    temp = []
-    now = [[0 for _ in range(4)] for _ in range(6)]
-    for i in range(len(maps)):
-        y = maps[i][1]
-        if not now[0][y]:
-            now[0][y] = 1
-            temp.append([0, y])
-        elif now[0][y]: # 2*1칸일 때 발생 가능 
-            now[1][y] = 1
-            temp.append([1, y])
-    flag = False
-    
-    while(1):
-        if flag:
-            break
-        maps = []
-        for i in range(len(temp)):
-            x, y = temp[i][0], temp[i][1]
-            nx, ny = x + dx[0], y + dy[0]
-            if 0 <= nx <= 5 and 0 <= ny <= 3:
-                if green[nx][ny] == 1: # 한칸 더 가면 겹침
-                    flag = True
-                else:
-                    x, y = nx, ny
-                    maps.append([x, y])
-            else:
-                flag = True   
-                
-        if not flag: # 겹치지 않았음 
-            temp = deepcopy(maps) # temp 최적화
-            
-        else: # 겹쳐버렸음 -> temp가 새로운 블럭의 최종 목적지가 됌 
-            for i in range(len(temp)):
-                green[temp[i][0]][temp[i][1]] = 1
-    
-    while(1):
-        bflag = False     
-        bang = []
-        for i in range(5, -1, -1):
-            if sum(green[i]) == 4: # 모든 행이 차게 되면
-                bflag = True
-                bang.append(i)
-                green[i] = [0, 0, 0, 0] # 터짐 
-                score += 1
-        
-        if not bflag:
-            break
-        else:
-            b = [0 for _ in range(6)]
-            for k in bang: # 행이 터진 경우에는
-                green[k] = [0, 0, 0, 0]
-                b[k] = 1
-                
-        for i in range(6):
-            if b[i] == 1: # i 행이 터진 행이라면 
-                green[0:i+1]  = [[0, 0, 0, 0]] + green[0:i]
-                
-    cnt = 0    
-    for i in range(0, 2): 
-        if green[i] != [0, 0, 0, 0]: # 연한 부분에 블럭이 있는 경우
-            cnt += 1
-    if cnt == 0:
-        pass
-    elif cnt == 1:
-        green =  [[0, 0, 0, 0]] + green[0:5]
-    else:
-        green =  [[0, 0, 0, 0]] + [[0, 0, 0, 0]] + green[0:4]
-                
-            
-             
-# 두 케이스가 동시에 존재하는 경우 -> 점수 획득 과정이 우선이다
-blue = [[0 for _ in range(4)] for _ in range(6)]
-green = [[0 for _ in range(4)] for _ in range(6)]
+# 하나의 행이 타일로 가득 차면 사라진다. 
+# 그 위에 있던 타일들이 한 칸씩 떨어짐 + 점수는 사라진 줄의 수만큼 획득한다. 
+# 0, 1번 행에 블록이 들어가면 해당 행만큼 사라진다. 
+green, blue = [[0 for _ in range(4)] for _ in range(6)], [[0 for _ in range(6)] for _ in range(4)]
 score = 0
-dx, dy = [1, -1, 0, 0], [0, 0, 1, -1]
-while(info):
-    x, y, t = info.pop()
-    if t == 1: # t = 1  -> x, y 
-        blue_block([[x, y]])
-        green_block([[x, y]])
-        
-    elif t == 2: # t = 2 -> x,y x,y+1
-        blue_block([[x, y], [x, y+1]])
-        green_block([[x, y], [x, y+1]])
-        
-    else: # t = 3 -> x, y x+1, y
-        blue_block([[x, y], [x+1, y]])
-        green_block([[x, y], [x+1, y]])
+while(comm):
+    t, x, y = comm.popleft()
+    drop_tile(t, x, y)
+    # 2. 초록과 파랑 중 한줄이 만들어진 것이 있다면 터트리고 그 위의 타일을 한칸씩 떨군다.
+    # 3. 맨위 두줄에 타일이 있다면 해당 줄만큼 아래에서 빼준다. 
+    check_line()
+
     
 print(score)
 print(sum(map(sum, blue)) + sum(map(sum, green)))
