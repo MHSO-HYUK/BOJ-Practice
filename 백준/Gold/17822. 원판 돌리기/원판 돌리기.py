823cdcd584733052662542dc4caad8b1dee8da30
MHSO-HYUK <88526660+MHSO-HYUK@users.noreply.github.com>
2022-04-28 21:16:45 +0900

    [Gold III] Title: 원판 돌리기, Time: 336 ms, Memory: 119460 KB -BaekjoonHub

--- 백준/Gold/17822. 원판 돌리기/원판 돌리기.py
+++ 백준/Gold/17822. 원판 돌리기/원판 돌리기.py
@@ -1,76 +1,75 @@
 # 17822 원판 돌리기
-from sys import stdin 
+from sys import stdin
 from collections import deque
-import sys
-def queueing(i, j, p):
-    dx, dy= [1, -1, 0, 0], [0, 0, 1, -1]
-    queue = deque([[i, j]])
-    visit[i][j] = 1
-    flag = False
+def bfs(x, y, num):
+    global flag
+    dx, dy = [1, -1, 0, 0], [0, 0, 1, -1]
+    queue = deque([[x, y]])
+    visit[x][y] = 1
+    cache = [[x, y]]
     while(queue):
         x, y = queue.popleft()
         for k in range(4):
-            a, b = x+dx[k], y+dy[k]
-            if 0 <= a <= n-1 and -1 <= b <= m:
-                if b == m:
-                    b = 0
-                if b == -1:
-                    b = m-1
-                if not visit[a][b] and one[a][b] == p:
-                    flag = True
-                    visit[a][b] = 1
-                    cache[a][b] = 1
-                    queue.append([a, b])
-    if flag:
-        cache[i][j] = 1
+            nx, ny = x+dx[k], y+dy[k]
+            if not (0 < nx <= n and 0 <= ny <= m-1):
+                if nx > n or nx < 1: # 원판이 아예 경계를 넘는 경우
+                    continue
+                if ny > m-1:
+                    ny -= m
+                elif ny < 0:
+                    ny += m
+                    
+            if circle[nx][ny] == num and visit[nx][ny] == 0:
+                visit[nx][ny] = 1
+                cache.append([nx,ny])
+                queue.append([nx,ny])
 
-n, m, t = map(int, stdin.readline().split())
-one = [[] for _ in range(n)]
+    if len(cache)>= 2:
+        flag = True
+        for a, b in cache:
+            circle[a][b] = 0
+
+# 원판의 회전은 독립적
+n, m, t =map(int, stdin.readline().split())
+circle = [0] + [[] for _ in range(n)]
 for i in range(n):
-    one[i] = list(map(int, stdin.readline().split()))
-command = list(list(map(int, stdin.readline().split())) for _ in range(t))
-command = command[::-1]
-while(command):
-    x, d, k = command.pop()
-    for i in range(n):
-        if not (i+1) % x:
-            if d == 0: # 시계 방향 k 만큼
-                one[i] = one[i][m-k:m] + one[i][0:m-k]
-            if d == 1: # 반시계 방향 
-                one[i] = one[i][k:m] + one[i][0:k]
-    
-    
-    visit = [[0]*m for _ in range(n)]
-    cache = [[0]*m for _ in range(n)]
-    for i in range(n):
-        for j in range(m):
-            if not visit[i][j] and one[i][j]:
-                queueing(i, j, one[i][j])
-                
-    k = False
-    for i in range(n):
-        for j in range(m):
-            if cache[i][j] :
-                k = True
-                one[i][j] = 0 
-                
-    if not k : # 지워지지 않음 
-        s, cnt = 0, 0
-        for i in range(n):
-            for j in range(m):
-                if one[i][j]:
-                    cnt += 1
-                    s += one[i][j]
-        if cnt:
-            s = s / cnt
-            for i in range(n):
-                for j in range(m):
-                    if one[i][j] > s:
-                        one[i][j] -= 1
-                    elif one[i][j] < s and one[i][j] != 0:
-                        one[i][j] += 1
-        else:
-            print(sum(map(sum, one)))
-            sys.exit()
+    circle[i+1] = list(map(int, stdin.readline().split()))
+comm = deque()
+for _ in range(t):
+    comm.append(list(map(int, stdin.readline().split())))
 
-print(sum(map(sum, one)))
+while(comm):
+    a,d,k = comm.popleft()
+    k %= m
+    for x in range(a, n+1, a):
+        if d == 0: # 시계방향
+            circle[x] = circle[x][-k:m] + circle[x][0:m-k] 
+        else: # 반시계방향 
+            circle[x] = circle[x][k:m] + circle[x][0:k]
+        # 원판에 수가 남아 있으면 인접하면서 수가 같은 것을 모두 찾는다.
+    # 그러한 수가 있다면 원판에서 인접하면서 같은 수를 모두 지움
+    # 없다면 원판에 적힌 수의 평균을 구하고 평균보다 작으면 1을 빼고 작으면 1을 더함
+    visit = [[0 for _ in range(m)] for _ in range(n+1)]
+    flag = False
+    cnt = 0
+    for i in range(1, n+1):
+        for j in range(m):
+            if not visit[i][j] and circle[i][j]:
+                cnt += 1
+                bfs(i, j, circle[i][j])
+
+    if not flag:
+        total = 0
+        for i in range(1, n+1):
+            total += sum(circle[i])
+        for i in range(1, n+1):
+            for j in range(m):
+                if circle[i][j]:
+                    if circle[i][j] > total / cnt:
+                        circle[i][j] -= 1
+                    elif circle[i][j] < total / cnt:
+                        circle[i][j] += 1
+ans = 0
+for i in range(1, n+1):
+    ans += sum(circle[i])
+print(ans)
