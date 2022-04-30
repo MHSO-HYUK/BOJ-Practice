21673f579304ba3e466bcbbcef58e14820a96eb9
MHSO-HYUK <88526660+MHSO-HYUK@users.noreply.github.com>
2022-04-30 20:45:37 +0900

    [Gold II] Title: 상어 중학교, Time: 296 ms, Memory: 120072 KB -BaekjoonHub

--- 백준/Gold/21609. 상어 중학교/상어 중학교.py
+++ 백준/Gold/21609. 상어 중학교/상어 중학교.py
@@ -2,98 +2,99 @@
 from sys import stdin
 from collections import deque
 import heapq
+def group(i, j, color):
+    # 1. 크기가 가장 큰 블록 그룹을 찾는다. > 무지개 많은 것 > 기준 블록의 행이 큰것 > 열이 큰 것
+    # 기준 블록 // 무지개가 아닌것 중 행의 번호 작은 것 > 열의 번호 작은 것
+    # 0 무지개 -1 검정 1 ~ m 색깔
+    queue = deque()
+    queue.append([i, j])
+    check = [[0 for _ in range(n)] for _ in range(n)]
+    check[i][j] = 1
+    cnt = 0
+    rainbow = 0
+    minr, minc = i, j
+    cache = []
+    while(queue):
+        cnt += 1
+        x, y = queue.popleft()
+        cache.append([x, y])
+        for k in range(4):
+            nx, ny = x + dx[k], y + dy[k]
+            if 0 <= nx <= n-1 and 0 <= ny <= n-1:
+                if not check[nx][ny]:
+                    if maps[nx][ny] == color: # 동일 색깔
+                        visit[nx][ny], check[nx][ny] = 1, 1
+                        minr = min(minr, nx)
+                        if minr == nx:
+                            minc = min(minc, ny)
+                        queue.append([nx, ny])
+                    if maps[nx][ny] == 0: #무지개
+                        rainbow += 1
+                        check[nx][ny] = 1
+                        queue.append([nx, ny])
+    if cnt >= 2:
+        return [-cnt, -rainbow, -minr, -minc, cache]
+    else:
+        return None
+
 def gravity(new_maps):
-    for i in range(n-1, -1, -1):
-        for j in range(n):
-            if new_maps[i][j] != -1 and new_maps[i][j] != -2: # 검정 혹은 빈칸이 아닌 경우
-                x = i + 1
+    for j in range(n):
+        for i in range(n-1 ,-1, -1):
+            if 0 <= new_maps[i][j]:
+                x, y = i, j
                 while(1):
-                    if x < n and new_maps[x][j] == -2: # 격자 내부이고 블록이 없는 경우
-                        x += 1 # 하강 
-                    else: # 바닥에 부딫히거나 블록이 있는 경우
-                        break # 정지
-                x -= 1
-                new_maps[x][j], new_maps[i][j] = new_maps[i][j], new_maps[x][j]
+                    nx, ny = x + 1, y
+                    if 0 <= nx <= n-1 and 0 <= ny <= n-1:
+                        if new_maps[nx][ny] == -2:
+                            new_maps[x][y], new_maps[nx][ny] = new_maps[nx][ny], new_maps[x][y]
+                            x, y = nx, ny
+                            continue
+                        else:
+                            break
+                    else:
+                        break
     return new_maps
 
 def rotate(new_maps):
-    ret_maps = []
+    ret = []
     for j in range(n-1, -1, -1):
-        cache = []
+        temp = []
         for i in range(n):
-            cache.append(maps[i][j])
-        ret_maps.append(cache)
-    return ret_maps
-    
-    
-def find_group(x, y):
-    # 그룹에는 일반 블록이 하나 이상 있어야 하며 색이 모두 같아야 한다.
-    # 검정 블록은 포함되면 안되고 무지개는 얼마든지 있어도 된다. 
-    # 그룹의 블록은 2개 이상 
-    # 블록 그룹의 기준 블록은 무지개 블록이 아닌 블록 중 행의 번호, 열의 번호 작은 순
-    # 1. 크기가 가장 큰 블록 그룹을 찾는다. 그러한 블록 그룹이 여러개 라면 포함된 
-    #무지개 블록수가 가장 많은 그룹 > 기준 블록의 행이 가장 큰 것 > 기준 블록의 열이 가장 큰 것
-    queue = deque()
-    rainbow, lr, lc = 0, x, y
-    check = [[0 for _ in range(n)] for _ in range(n)]
-    visit[x][y], check[x][y] = 1, 1
-    color = maps[x][y]
-    queue.append([x, y])
-    cache = []
-    cnt = 0
-    while(queue):
-        x, y = queue.popleft()
-        cache.append([x, y])
-        cnt += 1
-        for k in range(4):
-            nx, ny = x+dx[k], y+dy[k]
-            if 0 <= nx <= n-1 and 0 <= ny  <= n-1:
-                if not check[nx][ny]:
-                    if maps[nx][ny] == 0: # 인접한 무지개 블록
-                        check[nx][ny] = 1
-                        rainbow += 1
-                        queue.append([nx, ny])
-                    elif maps[nx][ny] == color: # 인접한 동일 색깔 블록 
-                        lr = min(lr, nx)
-                        lc = min(lc, ny)
-                        visit[nx][ny], check[nx][ny] = 1, 1
-                        queue.append([nx, ny])
-    
-    return [-cnt, -rainbow, -lr, -lc, cache]
-    
+            temp.append(new_maps[i][j])
+        ret.append(temp)
+    return ret
+
 n, m = map(int, stdin.readline().split())
 maps = list(list(map(int, stdin.readline().split())) for _ in range(n))
-dx, dy = [1, -1, 0 ,0], [0, 0, 1, -1]
-# 검정 -1 무지개 0 일반 1 ~ M // 빈칸 -2 로 정의! 
-# 그룹에는 일반 블록이 하나 이상 있어야 하며 색이 모두 같아야 한다.
-# 검정 블록은 포함되면 안되고 무지개는 얼마든지 있어도 된다. 
-# 그룹의 블록은 2개 이상 
-# 블록 그룹의 기준 블록은 무지개 블록이 아닌 블록 중 행의 번호, 열의 번호 작은 순
+dx, dy = [1, -1, 0, 0], [0, 0, 1, -1]
 score = 0
 while(1):
-    heap = []
+    # 1. 크기가 가장 큰 블록 그룹을 찾는다. > 무지개 많은 것 > 기준 블록의 행이 큰것 > 열이 큰 것
+    # 기준 블록 // 무지개가 아닌것 중 행의 번호 작은 것 > 열의 번호 작은 것
+    # 0 무지개 -1 검정 1 ~ m 색깔
     visit = [[0 for _ in range(n)] for _ in range(n)]
-    # 1. 크기가 가장 큰 블록 그룹을 찾는다. 그러한 블록 그룹이 여러개 라면 포함된 
-    #무지개 블록수가 가장 많은 그룹 > 기준 블록의 행이 가장 큰 것 > 기준 블록의 열이 가장 큰 것
+    heap = []
     for i in range(n):
         for j in range(n):
-            if not visit[i][j] and 1 <= maps[i][j]:
-                cnt, rainbow, lr, lc, cache = find_group(i, j)
-                if -cnt >= 2:
-                    heapq.heappush(heap, [cnt, rainbow, lr, lc, cache])
-
+            if 1 <= maps[i][j] and not visit[i][j]:
+                visit[i][j] = 1
+                temp = group(i, j, maps[i][j])
+                if temp:
+                    heapq.heappush(heap, temp)
     if heap:
-        cnt, rainbow, lr, lc, cache = heapq.heappop(heap)
+        cnt, rain, x, y, cache = heapq.heappop(heap)
     else:
         break
-# 2. 1에서 찾은 블록 그룹 제거 -> 블록수^2 획득
-    for a, b in cache:
-        maps[a][b] = -2
+    # 2. 블록 그룹의 블록을 제거 한다 이때 제곱점수 획득 (그룹 없으먄 종료)
+    for x, y in cache:
+        maps[x][y] = -2 # 빈칸으로 만듬
     score += cnt**2
-# 3. 중력 작용
+
+    # 3. 중력
     maps = gravity(maps)
-# 4. 90도 반시계 회전
+    # 4. 90도 반시계
     maps = rotate(maps)
-# 5.  중력 작용
+    # 5. 중력
     maps = gravity(maps)
+
 print(score)
